f146f809f21732a9d883e05cf13afc2837f63fd1
abidrahman2 <abidrahman2@gmail.com>
2014-11-05 12:11:53 +0530

    All ints to uchar - 40% speedup for small files and 10% speedup for largest file

--- sudoku3_optimized/plibsudoku.h
+++ sudoku3_optimized/plibsudoku.h
@@ -19,41 +19,44 @@
 #define nCOLS 9
 #define INTERVAL 5000
 
+typedef unsigned char uchar;
+typedef unsigned int uint;
+
 unsigned long nASSUMPTIONS = 0;
 
 typedef struct cell{
 //    int R, C;
-    char A[10];           // Designed for worst case
-    int N;              // Number of possible values
-    int idx;            // Which cell we processed last (TODO : Unnecessary, remove it)
+    uchar A[10];           // Designed for worst case
+    uchar N;              // Number of possible values, maximum is 9
+    uchar idx;            // Number of possible values right now
 //    int isEmpty;        // TODO : Unnecessary, remove it
 }Cell, *CellPtr;
 
-void printArray(int A[], int N)
+void printArray(uchar A[], uchar N)
 {
-    int i=0;
+    uchar i=0;
     for(i=0; i<N; ++i){
-        printf("%c ", A[i]);
+        printf("%d ", A[i]);
     }
     printf("\n");
 }
 
 void printCA(Cell CA[])
 {
-    int i,j;
+    uchar i,j;
     printf("i\tR\tC\tN\tidx\tisEmpty\t A\n");
     for(i=0; i<81; i++){
 //        printf("%d\t%d\t%d\t%d\t%d\t%d\t" , i, ROW(i), COL(i), CA[i].N, CA[i].idx, CA[i].isEmpty); 
         printf("%d\t%d\t%d\t%d\t%d\t" , i, ROW(i), COL(i), CA[i].N, CA[i].idx); 
         for(j=0; j<CA[i].N; ++j){
-            printf("%c ", CA[i].A[j]);
+            printf("%d ", CA[i].A[j]);
         }
     printf("\n");
     }
 }
-void printSudoku(int S[][nCOLS]) // TODO : Pretty print sudoku
+void printSudoku(uchar S[][nCOLS]) // TODO : Pretty print sudoku
 {
-    int i,j;
+    uchar i,j;
     for(i=0; i<nROWS; ++i){
         for(j=0; j<nCOLS; ++j){
             if(S[i][j] == 0)
@@ -65,46 +68,46 @@ void printSudoku(int S[][nCOLS]) // TODO : Pretty print sudoku
     }
 }
 
-void animateSudoku(int S[][nCOLS])
+void animateSudoku(uchar S[][nCOLS])
 {
     printf("\033c");
     printSudoku(S);
     usleep(INTERVAL);
 }
 
-int findPossibleValues(int S[][nCOLS], char A[], int cell_val)
+uchar findPossibleValues(uchar S[][nCOLS], uchar A[], uchar cell_val)
 {
-    int R = ROW(cell_val);
-    int C = COL(cell_val);
-    int count = 0;    
+    uchar R = ROW(cell_val);
+    uchar C = COL(cell_val);
+    uchar count = 0;    
 
     if (S[R][C]!=0){
         return count;
     }        
 
-    int tmp[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-    int i, j, tmp2;
+    uchar tmp[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    uchar i, j, tmp2;
     for(i=0; i<9; ++i){         // First check all values in a ROW    
         tmp2 = S[R][i];         // Take a number in Sudoku
-        tmp[tmp2] = '0';           // Clear it from tmp
+        tmp[tmp2] = 0;           // Clear it from tmp
     }
     
     for(i=0; i<9; ++i){
         tmp2 = S[i][C];         // Now check in a column, ie vertical scanning
-        tmp[tmp2] = '0';
+        tmp[tmp2] = 0;
     }
 
-    int BLK_R = B_ROW(cell_val);
-    int BLK_C = B_COL(cell_val);
+    uchar BLK_R = B_ROW(cell_val);
+    uchar BLK_C = B_COL(cell_val);
     for(i=BLK_R; i<BLK_R+3; ++i){
         for(j=BLK_C; j<BLK_C+3; ++j){
             tmp2 = S[i][j];
-            tmp[tmp2] = '0';
+            tmp[tmp2] = 0;
         }
     }
 
     for(i=1; i<10; i++){
-        if(tmp[i] != '0'){
+        if(tmp[i] != 0){
             A[count] = tmp[i];
             ++count;
         }
@@ -115,6 +118,7 @@ int findPossibleValues(int S[][nCOLS], char A[], int cell_val)
             
 // This function checks if a value inserted in a cell_val violates the Sudoku properties
 // Returns 1 if it is Fine, Else it returns 0
+/*
 int checkViolation(int S[][9], Cell CA[], int cell_val, int key)
 {
     int R = ROW(cell_val);
@@ -142,20 +146,21 @@ int checkViolation(int S[][9], Cell CA[], int cell_val, int key)
 
     return 1;
 }
+*/
 
-void updatePossibleValues(Cell CA[], int cell_val, char given_val)
+void updatePossibleValues(Cell CA[], uchar cell_val, uchar given_val)
 {
-    int R = ROW(cell_val);
-    int C = COL(cell_val);
+    uchar R = ROW(cell_val);
+    uchar C = COL(cell_val);
 
     //int tmp[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int i, j, k, tmp,  tmp2, n;
+    char i, j, k, tmp,  tmp2, n;
     for(i=0; i<9; ++i){         // First check all values in a ROW    
         tmp = R*9+i;
         n = CA[tmp].N;
         for(j=0; j<n; ++j){         //     
             if(CA[tmp].A[j] == given_val){        // Take a number in Sudoku
-                CA[tmp].A[j] = '0';           // Clear it from tmp
+                CA[tmp].A[j] = 0;           // Clear it from tmp
                 --CA[tmp].idx;
                 break;
             }
@@ -167,22 +172,22 @@ void updatePossibleValues(Cell CA[], int cell_val, char given_val)
         n = CA[tmp].N;
         for(j=0; j<n; ++j){
             if(CA[tmp].A[j] == given_val){
-                CA[tmp].A[j] = '0';         // Now check in a column, ie vertical scanning
+                CA[tmp].A[j] = 0;         // Now check in a column, ie vertical scanning
                 --CA[tmp].idx;
                 break;
             }
         }
     }
 
-    int BLK_R = B_ROW(cell_val);
-    int BLK_C = B_COL(cell_val);
+    uchar BLK_R = B_ROW(cell_val);
+    uchar BLK_C = B_COL(cell_val);
     for(i=BLK_R; i<BLK_R+3; ++i){
         for(j=BLK_C; j<BLK_C+3; ++j){
             tmp = i*9+j;
             n = CA[tmp].N;
             for(k=0; k<n; ++k){         //     
                 if(CA[tmp].A[k] == given_val){        // Take a number in Sudoku
-                    CA[tmp].A[k] = '0';           // Clear it from tmp
+                    CA[tmp].A[k] = 0;           // Clear it from tmp
                     --CA[tmp].idx;
                     break;
                 }
@@ -198,10 +203,10 @@ void updatePossibleValues(Cell CA[], int cell_val, char given_val)
 // left most element and partition the 
 // array based on this pivot
 //  Return the index of Pivot
-int Partition(int A[], Cell CA[], int l, int r){
-    
-    int pivot = A[l], wall = l, pivotN = CA[pivot].idx; 
-    int temp,i; 
+uchar Partition(uchar A[], Cell CA[], char l, char r){
+    uchar pivot = A[l], pivotN = CA[pivot].idx; 
+    char wall = l;
+    uchar temp,i; 
     for (i = l+1; i <= r; ++i)
         if(CA[A[i]].idx < pivotN) {
             ++wall; 
@@ -212,49 +217,50 @@ int Partition(int A[], Cell CA[], int l, int r){
     temp = A[l];
     A[l] = A[wall];
     A[wall] = temp;
+    //printf("%d\n",wall);
     return wall; 
 }
 
 
 //Write the quicksort recursive calls
 //This function performs  the quicksort
-void QuickSort(int A[], Cell CA[], int l, int r)
+void QuickSort(uchar A[], Cell CA[], char l, char r)
 {
     if(l<r) {
-        int pivot_pos = Partition(A, CA, l, r);
+        char pivot_pos = Partition(A, CA, l, r);
         QuickSort(A,CA,l,pivot_pos-1); 
         QuickSort(A,CA,pivot_pos+1,r); 
     }
 }
 
-int DFS(int S[][9], Cell CA[], int LIST[], int MaxSize)
+uchar DFS(uchar S[][9], Cell CA[], uchar LIST[], uchar MaxSize)
 {
     
-    int backTrace = 1;
-    int status = 0;
-    
-    int cellUT = LIST[0];                               // Cell Under Test
-    int R = ROW(cellUT);
-    int C = COL(cellUT);
-    int nPossibleValues = CA[cellUT].N;                     // Its number of possible values
-    int i, currSize, vstatus;
-    char testval;
+    uchar backTrace = 1;
+    uchar status = 0;
+
+    uchar cellUT = LIST[0];                               // Cell Under Test
+    uchar R = ROW(cellUT);
+    uchar C = COL(cellUT);
+    uchar nPossibleValues = CA[cellUT].N;                     // Its number of possible values
+    uchar i, currSize, vstatus;
+    uchar testval;
     for(i=0; i<nPossibleValues; ++i){
         testval = CA[cellUT].A[i];                          // Take each possible test values
-        if (testval == '0') 
+        if (testval == 0) 
             continue;
-        S[R][C] = (int)(testval-'0');            // assign it.
+        S[R][C] = testval;            // assign it.
         ++nASSUMPTIONS;
-        //    animateSudoku(S);
+            //animateSudoku(S);
         currSize = MaxSize-1;
         if(currSize == 0)
             return 1;                                   // if cell processed is last one, success !!!
         else{
             Cell CA2[81];
-            int LIST2[MaxSize]; 
+            uchar LIST2[MaxSize]; 
             memcpy(CA2, CA, 81*sizeof(Cell));
             updatePossibleValues(CA2, cellUT, testval);
-            memcpy(LIST2, &LIST[1], currSize*sizeof(int)); 
+            memcpy(LIST2, &LIST[1], currSize*sizeof(uchar)); 
             QuickSort(LIST2, CA2, 0, currSize-1); 
             //printArray(LIST2, currSize);
             if(CA2[LIST2[0]].idx>0){
--- sudoku3_optimized/pmain.c
+++ sudoku3_optimized/pmain.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
         printf("Correct Usage : ./a.out sudoku_file\n");
     }
 
-    char input[83];
+    uchar input[83];
     char* infile = argv[1];
     FILE *in = fopen(infile, "r");
     if(in == NULL){
@@ -23,24 +23,23 @@ int main(int argc, char* argv[])
         exit(1);
     }
     
-    int Sudoku[9][9];
+    uchar Sudoku[9][9];
     Cell CA[81];
     
-    int i, r, c, tmp,  nZeroCells=0;
+    uchar i, r, c, tmp,  nZeroCells=0;
     // Initialize the Sudoku Grid
     for(i=0; i<81; ++i){
         r = ROW(i);
         c = COL(i);
-        tmp = input[i]-'0';
-        Sudoku[r][c] = tmp;
+        tmp = input[i] - '0';
+        Sudoku[r][c] = tmp ;
     }
-
 
         // Initialize the CA array (TODO : Above loops can be merged, modify findPossibleValues for 1D array)
     for(i=0; i<81; ++i){
         r = ROW(i);
         c = COL(i);
-        tmp = input[i]-'0';
+        tmp = input[i] - '0';
         //CA[i].R = r; 
         //CA[i].C = c;
         CA[i].N = findPossibleValues(Sudoku, CA[i].A, i);
@@ -50,9 +49,8 @@ int main(int argc, char* argv[])
             ++nZeroCells;
     }
     
-
-    int j = 0;
-    int ListofEmptyCells[nZeroCells];
+    uchar j = 0;
+    uchar ListofEmptyCells[nZeroCells];
     for(i=0; i<81; ++i){
         if(input[i] == '0')
             ListofEmptyCells[j++] = i;
@@ -64,11 +62,11 @@ int main(int argc, char* argv[])
     start = clock();
     //printArray(ListofEmptyCells, nZeroCells);
     QuickSort(ListofEmptyCells, CA, 0, nZeroCells-1);
-   // printArray(ListofEmptyCells, nZeroCells);
-   // printSudoku(Sudoku);
-    int status = DFS(Sudoku, CA, ListofEmptyCells, nZeroCells );
+   // exit(1);
+    uchar status = DFS(Sudoku, CA, ListofEmptyCells, nZeroCells );
     end = clock();
 
+    //printf("====================================\n");
     printSudoku(Sudoku);
     printf(" status: %d\n nAssumptions: %lu\n time: %f\n", status, nASSUMPTIONS, (double)(end-start)/CLOCKS_PER_SEC);
     
